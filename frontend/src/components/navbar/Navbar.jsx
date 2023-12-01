31951c733d2cced7f3194daab375514016adde1e
Shahzaib <shahzaibafzalofficial@gmail.com>
2023-12-01 12:42:18 +0500

    frontend : make navbar responsive

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
+  const [isMobileMenueOpen, setIsMobileMenueOpen] = useState(false);
   useEffect(() => {
     const handleOutsideClick = (event) => {
       // Close the dropdown if it is open and the click is outside of it
@@ -22,43 +22,106 @@ const Navbar = () => {
     };
   }, [isDropdownOpen]);
   return (
-    <nav>
-      <span className="logo">
-        LearnWith<span className="name">Shebi</span>
-      </span>
+    <>
+      <nav>
+        <span className="logo">
+          LearnWith<span className="name">Shebi</span>
+        </span>
 
-      <div className="center">
-        <Link to="/">Home</Link>
-        <div className="dropdownWrapper">
-          <Link
+        <div className="center">
+          <Link to="/">Home</Link>
+          <div className="dropdownWrapper">
+            <Link
+              onClick={() => {
+                setIsDropdownOpen(!isDropdownOpen);
+              }}
+            >
+              Categories{" "}
+              <img
+                src="./down-arrow.png"
+                alt="arrow-down"
+                className="arrowIcon"
+              />
+            </Link>
+            <div className={`dropdown ${isDropdownOpen && "dropdownActive"} `}>
+              <Link to="/">JavaScript</Link>
+              <Link to="/">Python</Link>
+              <Link to="/">Express</Link>
+              <Link to="/">Node</Link>
+              <Link to="/">Information Technology</Link>
+            </div>
+          </div>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+
+        <div className="searchBox">
+          <input type="text" placeholder="Search..." />
+          <img src="./search.png" className="searchIcon" alt="" />
+        </div>
+
+        <div
+          className="hamburgerWrapper"
+          onClick={() => {
+            setIsMobileMenueOpen(true);
+          }}
+        >
+          <img
+            src="./hamburger.png"
+            alt="hamburger"
+            className="hamburgerIcon"
+          />
+        </div>
+
+        {/* MOBILE NAVBAR  */}
+      </nav>
+
+      <div
+        className={`mobileNavbarWrapper ${
+          isMobileMenueOpen && "mobileNavbarWrapperActive"
+        }`}
+      >
+        <div className="mobileNavbarContainer">
+          <img
+            src="./close.png"
+            alt=""
+            className="closeIcon"
             onClick={() => {
-              setIsDropdownOpen(!isDropdownOpen);
+              setIsMobileMenueOpen(false);
             }}
-          >
-            Categories{" "}
-            <img
-              src="./down-arrow.png"
-              alt="arrow-down"
-              className="arrowIcon"
-            />
-          </Link>
-          <div className={`dropdown ${isDropdownOpen && "dropdownActive"} `}>
-            <Link to="/">JavaScript</Link>
-            <Link to="/">Python</Link>
-            <Link to="/">Express</Link>
-            <Link to="/">Node</Link>
-            <Link to="/">Information Technology</Link>
+          />
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <div className="dropdownWrapper">
+            <Link
+              onClick={() => {
+                setIsDropdownOpen(!isDropdownOpen);
+              }}
+            >
+              Categories{" "}
+              <img
+                src="./down-arrow.png"
+                alt="arrow-down"
+                className="arrowIcon"
+              />
+            </Link>
+            <div className={`dropdown ${isDropdownOpen && "dropdownActive"} `}>
+              <Link to="/">JavaScript</Link>
+              <Link to="/">Python</Link>
+              <Link to="/">Express</Link>
+              <Link to="/">Node</Link>
+              <Link to="/">Information Technology</Link>
+              <Link to="/">JavaScript</Link>
+              <Link to="/">Python</Link>
+              <Link to="/">Express</Link>
+              <Link to="/">Node</Link>
+              <Link to="/">Information Technology</Link>
+            </div>
           </div>
         </div>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
       </div>
-
-      <div className="searchBox">
-        <input type="text" placeholder="Search..." />
-        <img src="./search.png" className="searchIcon" alt="" />
-      </div>
-    </nav>
+    </>
   );
 };
 
